79d74a56abb343464928437fbe439a09c4b84b21
Colab <colab@gmail.com>
2024-09-19 16:47:34 +0700

    update link for task

--- prisma/migrations/20240919094641_add_link_task/migration.sql
+++ prisma/migrations/20240919094641_add_link_task/migration.sql
@@ -2,11 +2,10 @@
   Warnings:
 
   - A unique constraint covering the columns `[user_telegram_id,task_id]` on the table `UserTask` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `link` to the `Task` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
-ALTER TABLE "Task" ADD COLUMN     "link" TEXT NOT NULL;
+ALTER TABLE "Task" ADD COLUMN     "link" TEXT NOT NULL DEFAULT '';
 
 -- CreateIndex
 CREATE UNIQUE INDEX "UserTask_user_telegram_id_task_id_key" ON "UserTask"("user_telegram_id", "task_id");
